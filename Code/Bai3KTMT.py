ea7b5b92d488facf53338d642dc3e900d7614cbf
NguyenThePhong <nguyenthephong_t65@hus.edu.vn>
2022-04-13 10:32:12 +0700

    Bai3 final code

--- Code/Bai3KTMT.py
+++ Code/Bai3KTMT.py
@@ -3,39 +3,42 @@ import time
 
 
 def main():
+    # define giá trị các nút bấm, đèn LED pin GPIO
     BT1 = 14
     BT2 = 4
     BT3 = 3
     BT4 = 2
     LED = 22
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)  # setup mode
+    # đặt các nút bấm là input, pull_up các nút bấm
     GPIO.setup(BT1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(BT2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(BT3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(BT4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # đặt đèn LED là output
     GPIO.setup(LED, GPIO.OUT)
-    GPIO.output(LED, GPIO.HIGH)
-    ispressBT4 = False
     ispressBT3 = False
+    ispressBT4 = False
     while True:
-        if GPIO.input(BT1) == GPIO.LOW:
-            print('BT1 press')
+        if GPIO.input(BT1) == GPIO.LOW:  # bấm nút 1
+            print("BT1 press")
             ispressBT3 = False
             GPIO.output(LED, GPIO.LOW)
             time.sleep(0.5)
-        if GPIO.input(BT2) == GPIO.LOW:
-            print('BT2 press')
+        if GPIO.input(BT2) == GPIO.LOW:  # bấm nút 2
+            print("BT2 press")
             ispressBT3 = False
-            GPIO.output(LED, GPIO.HIGH)
+            GPIO.output(LED, GPIO.LOW)
             time.sleep(0.5)
-        if GPIO.input(BT3) == GPIO.LOW:
-            print('BT3 press')
+        if GPIO.input(BT3) == GPIO.LOW:  # bấm nút 3
+            print("BT3 press")
             ispressBT3 = True
             GPIO.output(LED, GPIO.LOW)
             time.sleep(0.5)
-        if GPIO.input(BT4) == GPIO.LOW:
-            print('BT4 press')
+        if GPIO.input(BT4) == GPIO.LOW:  # bấm nút 4
+            print("BT4 press")
             ispressBT3 = False
+            # bấm lần 1 đèn sáng, bấm lần 2 đèn tắt
             if ispressBT4:
                 GPIO.output(LED, GPIO.HIGH)
                 ispressBT4 = False
@@ -46,8 +49,9 @@ def main():
                 ispressBT4 = True
                 time.sleep(0.5)
                 continue
+        # đèn sáng nhấp nháy khi bấm BT3
         if ispressBT3:
-            print('Durring Blinking')
+            print("Durring Blinking")
             if GPIO.input(LED) == GPIO.LOW:
                 GPIO.output(LED, GPIO.HIGH)
                 time.sleep(1)
@@ -57,6 +61,6 @@ def main():
 
 
 try:
-    main()
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    main()  # gọi hàm
+except KeyboardInterrupt:  # xử lí sự kiện Ctrl+C
+    GPIO.cleanup()  # giải phòng GPIO
